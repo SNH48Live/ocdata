8517497f8c7216663901049eab468f4fb419e489
SNH48 Live <snh48live@gmail.com>
2017-09-30 13:12:12 -0400

    Add initial version of the monitor script

--- lib/config.py
+++ lib/config.py
@@ -12,3 +12,14 @@ PLOTS_DIR = DATA_DIR / 'plots'
 for directory in (CONFIGS_DIR, DATA_DIR, NUMERIC_DIR, PLOTS_DIR):
     directory.mkdir(parents=True, exist_ok=True)
 del directory
+
+_default_fmt = '[%(asctime)s] %(levelname)s: %(message)s'
+_default_datefmt = '%Y-%m-%d %H:%M:%S'
+_handler = logging.StreamHandler()
+_handler.setFormatter(logging.Formatter(
+    fmt=_default_fmt,
+    datefmt=_default_datefmt,
+))
+logger = logging.getLogger('ChinaSNH48')
+logger.addHandler(_handler)
+logger.setLevel(logging.DEBUG)
