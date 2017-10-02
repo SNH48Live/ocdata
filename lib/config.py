ed351ebc727cf78f4c6e36cc82bfb3456e66439c
SNH48 Live <snh48live@gmail.com>
2017-10-02 13:56:00 -0400

    Move data/numeric to data/viewers

--- lib/config.py
+++ lib/config.py
@@ -6,10 +6,10 @@ HERE = pathlib.Path(__file__).resolve().parent
 ROOT = HERE.parent
 CONFIGS_DIR = ROOT / 'config'
 DATA_DIR = ROOT / 'data'
-NUMERIC_DIR = DATA_DIR / 'numeric'
+VIEWERS_DIR = DATA_DIR / 'viewers'
 PLOTS_DIR = DATA_DIR / 'plots'
 
-for directory in (CONFIGS_DIR, DATA_DIR, NUMERIC_DIR, PLOTS_DIR):
+for directory in (CONFIGS_DIR, DATA_DIR, VIEWERS_DIR, PLOTS_DIR):
     directory.mkdir(parents=True, exist_ok=True)
 del directory
 
